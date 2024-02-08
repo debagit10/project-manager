0db02069f6454d855619ace0a9906919a651fa18
Josiah Elias <josiahelias11@gmail.com>
2024-02-08 19:26:27 +0000

    fix: deleted node_module and package-lock.json

--- backend/routes/adminRoutes.js
+++
@@ -1,8 +0,0 @@
-const { addAdmin, removeAdmin } = require("../controllers/adminControllers");
-
-const router = require("express").Router();
-
-router.put("/add", addAdmin);
-router.put("/remove", removeAdmin);
-
-module.exports = router;
--- backend/routes/projectRoutes.js
+++
@@ -1,8 +0,0 @@
-const { addProject, getProject } = require("../controllers/projectControllers");
-
-const router = require("express").Router();
-
-router.post("/add", addProject);
-router.get("/get", getProject);
-
-module.exports = router;
